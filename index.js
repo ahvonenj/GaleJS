bc4543606de16e569ebf0751f546b4976cd4bd78
J <jonah.ahvonen@gmail.com>
2015-08-13 15:36:23 +0300

    Remove some tests

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ $(document).ready(function()
     gale = new Gale();
     gale.loadSourceFromJSON('translations.json', function()
     { 
-        gale.translateApp('english'); 
-        console.log(gale.getTranslationsById('post_first_content'));
-        console.log(gale.reverseTranslationLookup('Third subheader'));
+        gale.translateApp('english');
     });
     
     $('.translate_button').on('click', function()
